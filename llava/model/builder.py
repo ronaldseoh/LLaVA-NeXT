199cd44255d86547f6ea840e0f3b5765b780fd57
Ronald Seoh <ronaldseoh@icloud.com>
2024-07-01 15:26:31 +0900

    builder.py: minor

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -25,14 +25,20 @@ from llava.utils import rank0_print
 
 
 def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="auto", attn_implementation="flash_attention_2", customized_config=None, **kwargs):
-    kwargs = {"device_map": device_map}
-
     if load_8bit:
-        kwargs["load_in_8bit"] = True
+        kwargs['load_in_8bit'] = True
     elif load_4bit:
-        kwargs["quantization_config"] = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4")
+        kwargs["device_map"] = "auto"
+        kwargs['quantization_config'] = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.float16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type='nf4'
+        )
     else:
-        kwargs["torch_dtype"] = torch.float16
+        if device != "cuda":
+            kwargs['device_map'] = {"": device}
+        kwargs = {"device_map": device_map, **kwargs}
 
     if customized_config is not None:
         kwargs["config"] = customized_config
